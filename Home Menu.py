86cfd06ff13f629e2535800a2ec26835b1e4d4d7
nickv <n.vandepolder@student.han.nl>
2024-06-03 19:19:50 +0200

    3-6-2024 Added some comments.

--- Home Menu.py
+++ Home Menu.py
@@ -7,20 +7,22 @@ from Packages.Buddy_Pet_package import VirtualPetBuddy
 
 class Home:
     def __init__(self):
-        self.window = None
+        self.window = tk.Tk()
         self.home_label = None
         self.buddy_button = None
 
         self.setup_window()
         self.setup_widgets()
+
         self.window.mainloop()
 
+    # Sets up the home window.
     def setup_window(self):
         # Creates and configures the window.
-        self.window = tk.Tk()
         self.window.title('Home Menu')
         self.window.geometry('500x500')
 
+    # Sets up the widgets for the home screen.
     def setup_widgets(self):
         self.home_label = tk.Label(self.window, text='Home Menu')
         self.home_label.pack()
@@ -28,6 +30,7 @@ class Home:
         self.buddy_button = tk.Button(self.window, text='Buddy', command=self.show_buddy)
         self.buddy_button.pack()
 
+    # Triggers if the buddy button is pressed and the home screen needs to disappear.
     def show_buddy(self):
         self.window.destroy()
         vpet = VirtualPetBuddy()
